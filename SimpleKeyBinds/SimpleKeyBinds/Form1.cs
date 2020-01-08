4be794a09d44c042e39147c211bfa4869988a082
LevensLes <32346039+LevensLes@users.noreply.github.com>
2020-01-08 13:54:16 +0100

    Add files via upload

---
+++ SimpleKeyBinds/SimpleKeyBinds/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleKeyBinds
+{
+    public partial class Form1 : Form
+    {
+        private KeyboardHook keyboardHook = new KeyboardHook();
+        public bool IsToggled;
+        public Form1()
+        {
+            InitializeComponent();
+            keyboardHook.KeyUp += KeyboardHook_KeyUp;
+            keyboardHook.Install();
+
+        }
+        public KeyboardHook.VKeys KeyBind;
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            button1.Text = ">...<";
+            KeyBind = KeyboardHook.ConvertToBindable(e);
+            button1.Text = KeyBind.ToString();
+
+        }
+
+        private void KeyboardHook_KeyUp(KeyboardHook.VKeys key)
+        {
+            if (key == KeyBind || key == KeyboardHook.VKeys.SHIFT)
+            {
+                IsToggled = !IsToggled;
+            label1.Text = (IsToggled ? "TOGGLED: YES" : "TOGGLED: NO");
+            }
+           
+        }
+
+
+    }
+}
